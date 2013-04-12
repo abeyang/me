1404118cced911436abf761b578ed1f44d8ef1ea
Abe Yang <abeyang@gmail.com>
2013-04-12 15:58:41 -0700

    Started iPad Design.
    Also attached PSDs

--- CONFIG.php
+++ CONFIG.php
@@ -22,8 +22,15 @@ c::set('name', 'Kareo Labs');
 c::set('profession', 'Medical Billing');
 c::set('location', 'Irvine, CA');
 
+// <head> info
+
 c::set('description', 'Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.');
 c::set('keywords', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget urna mollis ornare vel eu leo.');
+
+// Analytics info
+// If you need to add anything, make sure to also modify the script inside header.php
+
+// TODO...
 
 // core functions
 
